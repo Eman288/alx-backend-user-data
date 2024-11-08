15dc64bed7fe8c3ac0d6f3cbc87c17ede492dcb1
Eman288 <emantamam107@gmail.com>
2024-11-08 15:28:16 +0000

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,4 +8,10 @@ def hash_password(password: str) -> bytes:
     """
     a function to hash the password and return the hashed one
     """
-    return bcrypt.hashpw(b"{password}}", bcrypt.gensalt(rounds=12))
+    return bcrypt.hashpw(b"{password}", bcrypt.gensalt(rounds=12))
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    a function to check if the password provided is correct or not
+    """
+    return bcrypt.checkpw(b"{password}", hashed_password)
